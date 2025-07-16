776b887be7ef585f9170a2f84456f451fde856ac
milan2501 <milanvasovic06@gmail.com>
2025-07-16 21:07:51 +0200

    commit after few days

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,11 +1,13 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { App } from './app';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+
 
 @NgModule({
   declarations: [
-    App
+    App,
+    BasicHighlightDirective,
   ],
   imports: [
     BrowserModule
--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'my-practices';
+  numbers = [1,2,3,4,5];
+  evenNumbers = [2,4];
+  oddNumbers = [1,3,5];
+  onlyOdd = false;
+
 }
